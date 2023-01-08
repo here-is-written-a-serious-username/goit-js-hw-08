997c060433ed6142a9941435d3b126ee86ac44b5
Lord-Bambucha <lordbambycha93@gmail.com>
2023-01-08 22:19:23 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,44 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
 
 
-alert("ну як?")
+
+const divGallery = document.querySelector(".gallery");
+
+// divGallery.addEventListener("click", onImgClick);
+
+divGallery.insertAdjacentHTML("beforeend", createGalleryMarkup());
+
+
+function createGalleryMarkup() {
+    const markup = galleryItems.map(imgMarkupMaker).join("");
+
+    function imgMarkupMaker({ preview, original, description }) {
+        return (
+            `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src= "${preview}"
+                alt= "${description}"
+                />
+            </a>
+            </li>`
+        );
+    }
+
+    return markup;
+}
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+});
